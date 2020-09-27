2bd6eaa90bd62284ac513e674ab66cbf131b035d
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-09-27 18:42:54 +0530

    basic process of socket started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
+const ENDPOINT = "wss://echo.websocket.org/";
+
+// https://www.valentinog.com/blog/socket-react/
+
+// io.set("origins", ENDPOINT);
 
 function App() {
+  const [response, setResponse] = useState("");
+
+  useEffect(() => {
+    const socket = io(ENDPOINT);
+
+    console.log("socket 1", socket);
+
+    socket.on("connect", (data) => {
+      console.log("a user connected");
+      setResponse(data);
+    });
+
+    socket.on("event", (data) => {
+      console.log("socket event", data);
+      setResponse(data);
+    });
+    socket.on("disconnect", (data) => {
+      console.log("socket disconnect", data);
+      setResponse(data);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <p>
+      It's <time dateTime={response}>{response}</time>
+    </p>
   );
 }
 
